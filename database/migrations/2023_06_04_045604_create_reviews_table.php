d963d960ad96aa758430b1ad221523b8d2e7e37b
fahminurcahya <fahminurcahya@gmail.com>
2023-06-04 12:02:10 +0700

    migrate seeder

--- database/migrations/2023_06_04_045604_create_reviews_table.php
+++ database/migrations/2023_06_04_045604_create_reviews_table.php
@@ -13,15 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('reviews', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('phone_number');
-            $table->string('avatar')->nullable();
-            $table->enum('role', ['admin', 'member']);
-            $table->timestamps();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('movie_id')->constrained('movies');
+            $table->integer('star');
+            $table->text('review');
+            $table->unique(['user_id', 'movie_id']);
+            $table->timestamps();        
         });
     }
 
@@ -32,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('reviews');
     }
 };
