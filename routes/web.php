a73554f777173a9aaaaf08b73d6d874d1805a52e
fahminurcahya <fahminurcahya@gmail.com>
2023-06-11 14:14:09 +0700

    login admin

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\MovieController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,8 +16,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [DashboardController::class, 'index'])->name('admin.dashboard');
-Route::group(['prefix' => 'movie'], function () {
+
+Route::get('/admin/login', [LoginController::class, 'index'])->name('admin.login');
+Route::post('/admin/login', [LoginController::class, 'authenticate'])->name('admin.login.auth');
+
+Route::group(['prefix' => 'admin', 'middleware' => ['admin.auth']], function () {
+
+    Route::get('/', [DashboardController::class, 'index'])->name('admin.dashboard');
+});
+
+
+Route::group(['prefix' => 'movie', 'middleware' => ['admin.auth']], function () {
     Route::get('/', [MovieController::class, 'index'])->name('admin.movie');
 
     Route::get('/create', [MovieController::class, 'create'])->name('admin.movie.create');
